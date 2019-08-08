8f8d659d6abdbf792d13975a15fcce6e19d2397d
zx1984 <1355952333@qq.com>
2019-08-09 08:20:15 +0900

    Add jsp-commands-and-actions

---
+++ codes/jsp-commands-and-actions/WebContent/index.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>案例：商品浏览记录的实现</title>
+</head>
+<body>
+	<h1>采用Model1(Jsp + Javabean)实现</h1>
+	<ul>
+		<li>实现DBHelper类</li>
+		<li>创建实体类</li>
+		<li>创建业务逻辑类(DAO)</li>
+		<li>创建页面层</li>
+	</ul>
+</body>
+</html>
---
+++ codes/jsp-commands-and-actions/src/com/jspcommands/util/DBHelper.java
@@ -0,0 +1,57 @@
+package com.jspcommands.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBHelper {
+  // 数据库驱动
+  // private static final String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
+  private static final String ORACLE_DRIVER = "oracle.jdbc.driver.OracleDriver"; //驱动
+  // 连接数据库的URL地址
+  // private static final String DB_URL = "jdbc:mysql://localhost:3306/shopping?useUnicode=true&charaterEncoding=UTF-8";
+  private static final String DB_URL = "jdbc:oracle:thin:@//127.0.0.1:1521/orcl";
+  // 数据库用户名
+  private static final String USER_NAME = "system";
+  // 数据库密码
+  private static final String PASSWORD = "admin";
+  // 
+  private static Connection conn = null;
+  
+  // 静态代码块负责加载驱动
+  static
+  {
+    try {
+      Class.forName(ORACLE_DRIVER);
+    } catch (ClassNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+  
+  // 单列模式返回数据库连接对象
+  public static Connection getConnection() {
+    if (conn == null) {
+      try {
+        conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+      } catch (SQLException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
+    }
+    return conn;
+  }
+  
+  public static void main(String[] args) {
+    try {
+      Connection conn = DBHelper.getConnection();
+      if (conn != null) {
+        System.out.println("数据库连接成功!");
+      } else {
+        System.out.println("数据库连接失败!");
+      }
+    } catch(Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
